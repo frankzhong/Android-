1091e137cd40b86fe87d7e2ae2146de89a7ff2f2
frankzhong <1033480608@qq.com>
2017-10-25 00:21:45 +0800

    添加了日期时间选择器

---
+++ 日期时间选择控件/src/README.md.txt
@@ -0,0 +1,7 @@
+[参考](http://blog.csdn.net/mxiaoyem/article/details/51565341)
+
+![](./img/img1.png)
+
+![](./img/img2.png)
+
+![](./img/img3.png)
---
+++ 日期时间选择控件/src/src/java/TimePickerDialogInterface.java
@@ -0,0 +1,10 @@
+package com.xfhk.android.client.imp;
+
+/**
+ * Created by zhongchao on 2017/10/24.
+ */
+
+public interface TimePickerDialogInterface {
+    public void positiveListener();
+    public void negativeListener();
+}
